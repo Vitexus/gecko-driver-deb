dec0d8a7590fdf0800e0947ec2f137eff6e53d4d
gamseb <gamseb@gmail.com>
2023-10-08 16:27:59 +0200

    Fixed download script for geckodriver

--- download.sh
+++ download.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-LATEST_URL=$(wget -qO- https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep -oP '"browser_download_url": "\K\(.*?linux64\.tar\.gz\)' | head -1)
 
-echo $LATEST_URL
+LATEST_URL=$(wget -qO- https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r '.assets[0].browser_download_url')
 
-wget "${LATEST_URL}" -O geckodriver.tar.gz
-tar -xvzf geckodriver.tar.gz 
+wget ${LATEST_URL} -O geckodriver.tar.gz
+tar -xvzf geckodriver.tar.gz
 mkdir debian/tmp
 mv geckodriver debian/tmp
 rm geckodriver.tar.gz
