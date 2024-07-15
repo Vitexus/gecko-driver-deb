e3ea52ed3236efd6725aedb8d788b89465a2eda7
CyberVitexus <info@vitexsoftware.cz>
2024-07-15 22:22:15 +0200

    Better build debug

--- upver.sh
+++ upver.sh
@@ -5,5 +5,7 @@ rm -f debian/changelog.dch
 # Get the latest release version
 LATEST_VERSION=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | sed 's/^v//')
 
+echo Fixing package version to  $LATEST_VERSION
+
 dch -r $LATEST_VERSION
 dch -v $LATEST_VERSION "New upstream release $LATEST_VERSION"
