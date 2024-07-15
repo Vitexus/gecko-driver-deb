d7b9ce96d9a127dc535c0904ae9f81ebd2e83d7b
CyberVitexus <info@vitexsoftware.cz>
2024-07-15 22:19:35 +0200

    Package update

--- download.sh
+++ download.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-LATEST_URL=$(wget -qO- https://api.github.com/repos/mozilla/geckodriver/releases/latest | jq -r '.assets[0].browser_download_url')
+# Get the latest release version
+LATEST_VERSION=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 
-wget ${LATEST_URL} -O geckodriver.tar.gz
-tar -xvzf geckodriver.tar.gz
-mkdir debian/tmp
-mv geckodriver debian/tmp
-rm geckodriver.tar.gz
+# Determine the current architecture
+ARCH=$(uname -m)
+if [ "$ARCH" == "x86_64" ]; then
+    ARCH="linux64"
+elif [ "$ARCH" == "aarch64" ]; then
+    ARCH="arm64"
+else
+    ARCH="linux32"
+fi
 
 
+# Construct the download URL
+URL="https://github.com/mozilla/geckodriver/releases/download/$LATEST_VERSION/geckodriver-$LATEST_VERSION-$ARCH.tar.gz"
+
+# Download the package
+mkdir  -p debian/tmp
+rm -rf debian/tmp/*
+wget "$URL" -O debian/tmp/geckodriver.tar.gz
+# Unpack the downloaded file
+tar -xf debian/tmp/geckodriver.tar.gz -C debian/tmp
+# Remove the tarball
+rm debian/tmp/geckodriver.tar.gz
---
+++ upver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f debian/changelog.dch
+
+# Get the latest release version
+LATEST_VERSION=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | sed 's/^v//')
+
+dch -r $LATEST_VERSION
+dch -v $LATEST_VERSION "New upstream release $LATEST_VERSION"
